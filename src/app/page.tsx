8e3f424e1929707d692ef7d2eb30ebadf7b63ee3
Andrei Iatan <andrei.iatan00@gmail.com>
2024-06-20 16:47:30 +0300

    add jest testing library and basic test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+  );
 }
