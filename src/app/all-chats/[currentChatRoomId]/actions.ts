4eeb2a800820a140555e59c206b96009eb39f838
Andrei Iatan <andrei.iatan00@gmail.com>
2024-06-24 08:55:28 +0300

    add use case for send message action

--- src/app/all-chats/[currentChatRoomId]/actions.ts
+++ src/app/all-chats/[currentChatRoomId]/actions.ts
@@ -3,30 +3,29 @@ import { CreateTextMessageInput } from "@/API";
 import { sendMessageUseCase } from "@/use-cases/textMessage";
 
 export default async function sendMessage(
+  chatRoomId: string,
+  ownerProfileId: string,
   formData: FormData,
-  // chatRoomId: string,
-  // ownerProfileId: string,
 ) {
   const message = formData.get("message") as string;
-  return "";
 
-  // if (!chatRoomId) {
-  //   throw Error("ChatRoomId not found");
-  // }
+  if (!chatRoomId) {
+    throw Error("ChatRoomId not found");
+  }
 
-  // if (!ownerProfileId) {
-  //   throw Error("OwnerProfileId not found");
-  // }
+  if (!ownerProfileId) {
+    throw Error("OwnerProfileId not found");
+  }
 
-  // if (!message) {
-  //   throw Error("Message not found");
-  // }
+  if (!message) {
+    throw Error("Message not found");
+  }
 
-  // const textMessage: CreateTextMessageInput = {
-  //   chatRoomId,
-  //   ownerProfileId,
-  //   message,
-  // };
+  const textMessage: CreateTextMessageInput = {
+    chatRoomId,
+    ownerProfileId,
+    message,
+  };
 
-  // await sendMessageUseCase(textMessage);
+  await sendMessageUseCase(textMessage);
 }
--- src/app/all-chats/[currentChatRoomId]/page.tsx
+++ src/app/all-chats/[currentChatRoomId]/page.tsx
@@ -1,19 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useParams } from "next/navigation";
 import sendMessageAction from "./actions";
-import { useRouter } from "next/router";
+import ChatHistory from "./ChatHistory";
 
-export default function CurrentChat() {
-  // console.log(currentChatRoomId.query.currentChatRoomId);
+export default async function CurrentChat({
+  params,
+}: {
+  params: { currentChatRoomId: string };
+}) {
+  const { currentChatRoomId } = params;
 
   return (
     <main className="flex w-full flex-col justify-between border p-2">
-      {/* <ChatHistory /> */}
-      {/* <form action={} className="flex w-full gap-2">
+      <ChatHistory />
+      <form
+        action={sendMessageAction.bind(
+          null,
+          currentChatRoomId,
+          "b7361082-5c82-4c0b-98f1-0ef0fb7bb895",
+        )}
+        className="flex w-full gap-2"
+      >
         <Input name="message" className="w-full" />
         <Button type="submit">Send</Button>
-      </form> */}
+      </form>
     </main>
   );
 }
