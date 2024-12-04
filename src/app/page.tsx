7bc9b1fbcef4d991d89945f9b532438b905e3af2
Andrei Iatan <andrei.iatan00@gmail.com>
2024-12-04 20:42:50 +0200

    change width of register buttons on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,19 @@ export default function HomePage() {
     <main className="bg-gamboge-100">
       <div className="flex h-[72px] items-center justify-between p-4">
         <Text className="text-2xl font-bold">CrossMeasure</Text>
-        <SecondaryButton text="Log in" style="hidden h-10 w-32 lg:block" />
+        <SecondaryButton text="Log in" style="hidden h-10 w-32 md:block" />
       </div>
       <div className="flex h-[calc(100dvh-72px)] items-center justify-center">
         <div className="flex flex-col items-center gap-8 px-4">
           <Text className="text-3xl font-bold md:text-6xl lg:text-8xl">
             Find you musical peer.
           </Text>
-          <div className="flex flex-col items-center gap-2">
-            <PrimaryButton text="Create account" style="w-52 h-12" />
-            <SecondaryButton text="Log in" style="block w-52 h-12 lg:hidden" />
+          <div className="flex w-full flex-col items-center gap-2">
+            <PrimaryButton text="Create account" style="md:w-52 h-12 w-full" />
+            <SecondaryButton
+              text="Log in"
+              style="block w-full h-12 md:hidden"
+            />
           </div>
         </div>
       </div>
--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -8,7 +8,7 @@ export default function PrimaryButton(props: {
 }) {
   return (
     <Button
-      className={`${props.style ?? ""} to-gamboge-500 h-full w-full cursor-pointer rounded-3xl bg-gradient-to-r from-red-500 text-lg`}
+      className={`${props.style ?? ""} to-gamboge-500 cursor-pointer rounded-3xl bg-gradient-to-r from-red-500 text-lg`}
       onClick={props.onClick}
     >
       {props.text}
