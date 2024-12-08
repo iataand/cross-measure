141ac3ae388d2928c0da7062e9f5d21bfe8d536a
Andrei Iatan <andrei.iatan00@gmail.com>
2024-12-08 23:46:06 +0200

    implement route guarding using middleware and firebase-auth-edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,53 @@
-import { auth } from "firebase-admin";
-import { getAuth } from "firebase/auth";
 import { NextRequest, NextResponse } from "next/server";
-// import admin from "./firebaseAdminConfig";
+import {
+  authMiddleware,
+  redirectToHome,
+  redirectToLogin,
+} from "next-firebase-auth-edge";
+import { env } from "./env";
+import { serverConfig } from "~/lib/configs/firebase-config";
 
-export async function middleware(req: NextRequest) {
-  console.log("from middleware");
-  // const token = req.cookies.get("csrfToken");
-  // admin.auth().;
-  // console.log(token);
-  // const verifiedToken = await admin.auth().verifyIdToken();
+const PUBLIC_PATHS = ["/register", "/login"];
 
-  try {
-    // await getAuth().verifyIdToken(token);
-    return NextResponse.next(); // Allow the request to proceed
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+export async function middleware(request: NextRequest) {
+  return authMiddleware(request, {
+    loginPath: "/api/login",
+    logoutPath: "/api/logout",
+    apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    cookieName: serverConfig.cookieName,
+    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+    cookieSerializeOptions: serverConfig.cookieSerializeOptions,
+    serviceAccount: serverConfig.serviceAccount,
+    handleValidToken: async ({ token, decodedToken }, headers) => {
+      if (PUBLIC_PATHS.includes(request.nextUrl.pathname)) {
+        return redirectToHome(request);
+      }
+
+      return NextResponse.next({
+        request: {
+          headers,
+        },
+      });
+    },
+    handleInvalidToken: async (reason) => {
+      console.info("Missing or malformed credentials", { reason });
+
+      return redirectToLogin(request, {
+        path: "/",
+        publicPaths: PUBLIC_PATHS,
+      });
+    },
+    handleError: async (error) => {
+      console.error("Unhandled authentication error", { error });
+
+      return redirectToLogin(request, {
+        path: "/",
+        publicPaths: PUBLIC_PATHS,
+      });
+    },
+  });
 }
 
 export const config = {
-  matcher: ["/create-profile/:uid*"], // Apply middleware to these routes
+  matcher: ["/api/login", "/api/logout", "/create-profile/100"],
 };
