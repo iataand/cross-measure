cf63fb5400a58d33e2d2a4153d6e60afc02d5197
Andrei Iatan <andrei.iatan00@gmail.com>
2024-12-08 23:44:02 +0200

    refactor firebase-auth-edge config file

--- src/app/api/login/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET(req: Request, res: Response) {
-  // const { accessToken } = await req.json();
-  // console.log(accessToken);
-  // console.log(req.headers.);
-  return new Response("hi");
-  // const idToken = req.body.idToken.toString();
-}
