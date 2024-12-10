3fc0b2d4d7ece3a8e556724cd79d64b1ab6abb46
Andrei Iatan <andrei.iatan00@gmail.com>
2024-12-10 12:33:47 +0200

    create a basic layout for band profile creation

--- src/app/create-profile/band/page.tsx
+++ src/app/create-profile/band/page.tsx
@@ -1,9 +1,34 @@
-export default async function CreateProfile({
-  params,
-}: {
-  params: Promise<{ uid: string }>;
-}) {
-  const uid = (await params).uid;
+import { IconPlus } from "@tabler/icons-react";
 
-  return <div>asdasd</div>;
+export default async function CreateProfile() {
+  return (
+    <div>
+      <h2 className="py-6 text-center text-2xl font-bold">
+        Create Band Account
+      </h2>
+      <div className="flex justify-center">
+        <form className="flex w-full max-w-[900px] flex-wrap gap-12">
+          <div className="min-w-[300px] flex-1">
+            <p className="py-2">Band Name</p>
+            <input className="h-12 w-full border-2 border-gray-700 bg-black p-1" />
+            <p className="py-2">Email</p>
+            <input className="h-12 w-full border-2 border-gray-700 bg-black p-1" />
+            <p className="py-2">Bio</p>
+            <textarea
+              className="h-32 w-full border-2 border-gray-700 bg-black p-1"
+              placeholder="a few words about the band..."
+            />
+            <p className="py-2">Interested in</p>
+            <div className="w-20">
+              <div className="flex w-full min-w-40 cursor-pointer items-center justify-center gap-1 rounded-3xl border-2 border-gray-600 py-2 hover:bg-gray-800">
+                <IconPlus />
+                Add Genres
+              </div>
+            </div>
+          </div>
+          <div className="min-w-[300px] flex-1">videos here</div>
+        </form>
+      </div>
+    </div>
+  );
 }
