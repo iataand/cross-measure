76408cc8a02d7dcb12c3305656a5221c61fc3c54
Andrei Iatan <andrei.iatan00@gmail.com>
2024-12-11 10:56:41 +0200

    change modal open trigger to button for improved accesibility

--- src/components/GenresModal.tsx
+++ src/components/GenresModal.tsx
@@ -10,27 +10,24 @@ export default function GenresModal({ allGenres }: { allGenres: string[] }) {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <div className="w-20">
-          <div className="flex w-full min-w-40 cursor-pointer items-center justify-center gap-1 rounded-3xl border-2 border-gray-600 py-2 hover:bg-gray-800">
-            <IconPlus />
-            Add Genres
-          </div>
-        </div>
+        <button className="flex w-20 min-w-40 cursor-pointer items-center justify-center gap-1 rounded-3xl border-2 border-gray-600 py-2 hover:bg-gray-800">
+          <IconPlus />
+          Add Genres
+        </button>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black/50" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-1/2 top-1/2 flex h-[100vh] w-[100dvw] -translate-x-1/2 -translate-y-1/2 flex-col gap-6 rounded-md bg-[#111418] p-12 text-center text-white shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none md:max-h-[600px] md:max-w-[650px]">
           <Dialog.Title className="m-0 text-3xl font-medium">
-            What kind of music are you into?
+            <label>What kind of music are you into?</label>
           </Dialog.Title>
-          <Dialog.Description>
-            <input
-              type="text"
-              className="bg-black p-2"
-              onChange={(e) => setSearchGenre(e.target.value)}
-            />
-          </Dialog.Description>
-
+          <Dialog.Description />
+          <input
+            type="text"
+            className="bg-black p-2"
+            onChange={(e) => setSearchGenre(e.target.value)}
+            placeholder="search genre"
+          />
           <div className="no-scrollbar flex flex-wrap justify-center gap-1 overflow-y-scroll">
             {genres.map((genre, index) => (
               <div
