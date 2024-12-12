8bbcfe74bbd971f2a9de65049132485d55fc10fb
Andrei Iatan <andrei.iatan00@gmail.com>
2024-12-12 15:43:34 +0200

    add location api key to .env

--- src/env.js
+++ src/env.js
@@ -11,6 +11,7 @@ export const env = createEnv({
     FIREBASE_PROJECT_ID: z.string(),
     FIREBASE_CLIENT_EMAIL: z.string(),
     FIREBASE_PRIVATE_KEY: z.string(),
+    LOCATION_API_KEY: z.string(),
   },
 
   /**
@@ -35,6 +36,7 @@ export const env = createEnv({
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    LOCATION_API_KEY: process.env.LOCATION_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
