135b445df59ec30c264a3af6a6f84be521e31ef3
Andrei Iatan <andrei.iatan00@gmail.com>
2025-01-19 11:21:06 +0200

    add card for profile with image, name, location and email

--- src/app/profile/[profileId]/page.tsx
+++ src/app/profile/[profileId]/page.tsx
@@ -1,5 +1,14 @@
 import { getProfileByProfileIdAction } from "./_actions/get-profile-by-profileId.action";
 import Header from "./header";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import Image from "next/image";
 
 export default async function ProfilePage({
   params,
@@ -11,8 +20,24 @@ export default async function ProfilePage({
 
   return (
     <div>
-      <Header />
-      Band Name: {profile.bandName}
+      <Header name={profile.bandName} />
+      <Card className="m-auto mt-4 max-w-[800px]">
+        <CardHeader>
+          <div className="flex gap-4">
+            {/* <Image width={100} height={100} alt="Profile image" /> */}
+            <div className="h-[150px] w-[130px] border"></div>
+            <div className="flex flex-col justify-between">
+              <CardTitle>{profile.bandName}</CardTitle>
+              <div className="grid grid-cols-2 gap-2">
+                <p className="text-muted-foreground">location</p>
+                {profile.location}
+                <p className="text-muted-foreground">email</p>
+                {profile.email}
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+      </Card>
     </div>
   );
 }
