25b34becf8fc54576f63f99e0aa5b0631309568e
Andrei Iatan <andrei.iatan00@gmail.com>
2025-01-23 07:37:34 +0200

    add env variables in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    googleClientId: process.env.GOOGLE_CLIENTID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,15 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Google", function () {
+  beforeEach(function () {});
+
+  it("shows onboarding", function () {
     cy.visit("http://localhost:3000/");
+    cy.contains("Log in").should("be.visible");
+    // cy.loginByGoogleApi();
+    cy.contains(/create account/i).click();
+    // cy.loginByGoogleApi();
+    cy.contains(/login with google/i).click();
+    cy.window().then((win) => {
+      // win is the remote window
+    });
   });
 });
