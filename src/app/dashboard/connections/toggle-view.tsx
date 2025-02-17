82190d0e0836a549525f1c2f30b7868bc3416ce4
Andrei Iatan <andrei.iatan00@gmail.com>
2025-02-17 12:43:52 +0200

    fix connections not showing bug

--- src/app/dashboard/connections/toggle-view.tsx
+++ src/app/dashboard/connections/toggle-view.tsx
@@ -13,7 +13,7 @@ import { Button } from "~/components/ui/button";
 import { BandProfile } from "~/data-access/profiles/create-band-profile";
 
 type PropTypes = {
-  profiles: BandProfile[];
+  connections: { profile: BandProfile; id: number }[];
 };
 
 export default function ToggleViews(props: PropTypes) {
@@ -39,27 +39,27 @@ export default function ToggleViews(props: PropTypes) {
         <>
           <h1 className="mb-4 text-center text-2xl">All of your connections</h1>
           <ul>
-            {props.profiles.length === 0 && (
+            {props.connections.length === 0 && (
               <p className="text-center">You don't have any connections yet.</p>
             )}
-            {props.profiles?.map((profile) => (
-              <li key={profile.userId}>
+            {props.connections?.map((connection) => (
+              <li key={connection.profile.userId}>
                 <Card className="m-auto mt-4 max-w-[750px] px-2">
                   <CardHeader>
                     <div className="flex justify-between">
-                      <CardTitle>{profile.bandName}</CardTitle>
+                      <CardTitle>{connection.profile.bandName}</CardTitle>
                       <Button variant="destructive">Delete connection</Button>
                     </div>
-                    <CardDescription>{profile.bio}</CardDescription>
+                    <CardDescription>{connection.profile.bio}</CardDescription>
                   </CardHeader>
                   <CardContent className="flex grid-cols-2 flex-wrap gap-2 md:grid">
                     <p className="text-muted-foreground">Email</p>
-                    <p>{profile.email}</p>
+                    <p>{connection.profile.email}</p>
                     <p className="text-muted-foreground">Location</p>
-                    <p>{profile.location}</p>
+                    <p>{connection.profile.location}</p>
                     <p className="text-muted-foreground">Genres</p>
                     <ul className="flex flex-wrap gap-1">
-                      {profile.genres?.map((genre) => (
+                      {connection.profile.genres?.map((genre) => (
                         <li
                           key={genre}
                           className="cursor-pointer rounded-3xl border-2 border-gray-700 px-2 py-1"
--- src/data-access/connections/get-connections-by-profile-id.ts
+++ src/data-access/connections/get-connections-by-profile-id.ts
@@ -14,22 +14,22 @@ export type ConnectionType = {
 
 export const getConnections = cache(
   async (): Promise<{ profile: BandProfile; id: number }[]> => {
-    const authProfileUid = await getAuthUid();
+    const userData = await getAuthUid();
 
-    if (!authProfileUid) {
+    if (!userData?.user_id) {
       throw Error("User is not authenticated");
     }
 
     const res = await db.query.connections.findMany({
       with: { secondProfile: true, firstProfile: true },
       where: or(
-        eq(connections.firstProfile, authProfileUid),
-        eq(connections.secondProfile, authProfileUid),
+        eq(connections.firstProfile, userData.user_id),
+        eq(connections.secondProfile, userData.user_id),
       ),
     });
 
     const profiles = res.map((connection: ConnectionType) => {
-      if (connection.firstProfile.userId === authProfileUid) {
+      if (connection.firstProfile.userId === userData.user_id) {
         return {
           profile: connection.secondProfile,
           id: connection.id,
