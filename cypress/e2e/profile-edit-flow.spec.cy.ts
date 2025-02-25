57082b61ee5a19a176aa45ce87ceca03d774d281
Andrei Iatan <andrei.iatan00@gmail.com>
2025-02-25 16:05:26 +0200

    add e2e tests for create and edit profile

--- cypress/e2e/profile-edit-flow.spec.cy.ts
+++ cypress/e2e/profile-edit-flow.spec.cy.ts
@@ -1,11 +1,10 @@
-describe("Profile create flow", function () {
-  before(() => {
+describe("Create and edit a test profile", () => {
+  beforeEach(() => {
     cy.login();
     cy.setCookie("sessionCookie", Cypress.env("SESSION_COOKIE"));
   });
 
-  it("should create a new account with a test email and delete it when it's done", async function () {
-    //TODO: add testing for genres as well
+  it("create a test profile and then edit it and assert the new changes", () => {
     cy.visit("/create-profile/band");
 
     cy.get('input[name="bandName"]').type("Test band name");
@@ -19,12 +18,26 @@ describe("Profile create flow", function () {
     cy.get("[data-cy='submit']").as("btn").click();
     cy.get("@btn").should("not.be", "visible");
 
-    cy.contains('[data-cy="band-name"]', "Test band name");
-
     cy.contains(
-      '[data-cy="band-bio"]',
+      'textarea[name="bio"]',
       "This is a test bio. We are testing and this is a bio.",
     );
+
+    cy.get('[data-cy="edit-profile-button"]').as("btn").click();
+    cy.get("@btn").should("not.be", "visible");
+
+    cy.get('[data-cy="edit-name-input"]').type("Name band test");
+
+    cy.get('[data-cy="edit-bio-input"]')
+      .clear()
+      .type("test bio numero 2 goes here");
+
+    cy.get('[data-cy="save-profile"]').as("btn").click();
+    cy.get("@btn").should("not.be", "visible");
+
+    cy.contains('[data-cy="band-name"]', "Name band test");
+
+    cy.contains('[data-cy="band-bio"]', "test bio numero 2 goes here");
 
     cy.get('[data-cy="edit-profile-button"]').as("btn").click();
     cy.get("@btn").should("not.be", "visible");
